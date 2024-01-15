5ce5f2361b481abe958fed9a1115a38c02fd4621
ShiaoC <82642151+ShiaoC@users.noreply.github.com>
2024-01-16 02:15:19 +0800

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 蕭張媛 on 2024/1/16.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
